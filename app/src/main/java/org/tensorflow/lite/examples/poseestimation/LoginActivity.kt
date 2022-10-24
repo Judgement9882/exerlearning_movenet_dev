0650667ae2619cd546fabe61b9a713aa0c7befa4
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-10-24 20:59:26 +0900

    for secret
    for secret

--- app/src/main/java/org/tensorflow/lite/examples/poseestimation/LoginActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/poseestimation/LoginActivity.kt
@@ -34,7 +34,7 @@ class LoginActivity : AppCompatActivity() {
 
         // 2. 레트로핏 생성
         val retrofit = Retrofit.Builder()
-            .baseUrl("http://ec2-13-57-234-0.us-west-1.compute.amazonaws.com:3000")
+            .baseUrl("your link")
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
 
